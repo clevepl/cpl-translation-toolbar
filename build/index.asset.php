73da2867cb9ba584570e51a345b446eec9e9e549
Will Skora <skorasaurus@gmail.com>
2024-03-28 12:38:12 -0400

    initial commit

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '4555bc6129308963cb33');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '8f89ebb201e2bc95d17a');
